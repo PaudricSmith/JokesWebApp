372a75e5194b1e3a2cb5024824b0f80503fb6e45
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2023-03-04 16:04:09 +0000

    Changed git location

---
+++ Data/Migrations/20230304144648_initialsetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Joke");
+        }
+    }
+}
